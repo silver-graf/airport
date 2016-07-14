72b63b3cb78578541098eaddbf3163c3791765cd
Селивановский Иван <silver.graf@outlook.com>
2016-07-14 13:59:20 +0300

    init

--- airport/airport/Properties/AssemblyInfo.cs
+++ airport/airport/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test")]
+[assembly: AssemblyTitle("airport")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test")]
+[assembly: AssemblyProduct("airport")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d2b46a8a-a110-4338-8bc9-6cb2c2f103a0")]
+[assembly: Guid("645a6c90-8397-4f00-9498-455d742564f9")]
 
 // Version information for an assembly consists of the following four values:
 //
